339a7d592b236e239a4708f4a46f2a4abee3f852
alvansga <zralvansga97@gmail.com>
2021-06-06 13:19:21 +0700

    move player (proper way: frame rate concerned)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.core.window import Window
+from kivy.clock import Clock
 
 class GameWidget(Widget):
 	def __init__(self,**kwargs):
@@ -9,33 +10,50 @@ class GameWidget(Widget):
 		
 		self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
 		self._keyboard.bind(on_key_down = self._on_key_down)
+		self._keyboard.bind(on_key_up = self._on_key_up)
 		
 		with self.canvas:
 			self.player = Rectangle(source="player2.png",pos=(0,0),size=(100,100))
 			
+		self.keysPressed = set()
+		
+		Clock.schedule_interval(self.move_step,0)
+		
 	def _on_keyboard_closed(self):
 		self._keyboard.unbind(on_key_down = self._on_key_down)
+		self._keyboard.unbind(on_key_up = self._on_key_up)
 		self._keyboard = None
 		
 	def _on_key_down(self, keyboard, keycode, text, modifiers):
-		print("key down:",text)
+		# print("key down:",text)
+		self.keysPressed.add(text)
+		
+	def _on_key_up(self,keyboard,keycode):
+		text = keycode[1]
+		if text in self.keysPressed:
+			self.keysPressed.remove(text)
+			
+	def move_step(self,dt):
 		
 		cur_x = self.player.pos[0]
 		cur_y = self.player.pos[1]
 		
-		if text == "w":
-			cur_y += 5
-		if text == "s":
-			cur_y -= 5
+		step_size = 500 * dt
+		
+		if "w" in self.keysPressed:
+			cur_y += step_size
+		if "s" in self.keysPressed:
+			cur_y -= step_size
 			
-		if text == "a":
-			cur_x -= 5
-		if text == "d":
-			cur_x += 5
+		if "a" in self.keysPressed:
+			cur_x -= step_size
+		if "d" in self.keysPressed:
+			cur_x += step_size
 			
 		new_x = cur_x
 		new_y = cur_y
 		self.player.pos = (new_x,new_y)
+		
 		
 class MyApp(App):
 	def build(self):
