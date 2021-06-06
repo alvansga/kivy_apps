7a5769cb56c76638e9fd9774dff6645da3b92e1e
alvansga <zralvansga97@gmail.com>
2021-06-06 12:59:22 +0700

    drawing png on rectangle, detect keyboard down

---
+++ main.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window
+
+class GameWidget(Widget):
+	def __init__(self,**kwargs):
+		super().__init__(**kwargs)
+		
+		self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
+		self._keyboard.bind(on_key_down = self._on_key_down)
+		
+		with self.canvas:
+			Rectangle(source="player2.png",pos=(0,0),size=(100,100))
+			
+	def _on_keyboard_closed(self):
+		self._keyboard.unbind(on_key_down = self._on_key_down)
+		self._keyboard = None
+		
+	def _on_key_down(self, keyboard, keycode, text, modifiers):
+		print("key down detect")
+		
+class MyApp(App):
+	def build(self):
+		return GameWidget()
+			
+if __name__ == "__main__":
+	app = MyApp()
+	app.run()
